9b176fa3bc75d8a23c28bf316959fa2a49fde056
Samuel Mbugua Nyambura <samnmbuguah@gmail.com>
2024-12-04 17:20:29 +0300

    🗃️ Refactor drizzle.config.ts to improve database configuration and type safety. Updated to use connectionString for database URL and changed dialect to 'pg'. Added dotenv support for environment variable management.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,13 @@
-import { defineConfig } from "drizzle-kit";
+import type { Config } from "drizzle-kit";
+import * as dotenv from "dotenv";
+dotenv.config();
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL, ensure the database is provisioned");
-}
-
-export default defineConfig({
-  out: "./migrations",
+export default {
   schema: "./db/schema.ts",
-  dialect: "postgresql",
+  out: "./migrations",
+  driver: "pg",
+  dialect: "pg",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL!,
   },
-});
+} satisfies Config;
