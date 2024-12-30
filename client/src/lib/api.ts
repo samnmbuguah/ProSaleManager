8b1887f4035d944e608a1e2527231c36af923bcb
Samuel <Samnmbuguah@prosale.com>
2024-12-30 08:42:37 +0000

    fix deployment :bug

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "/api",
+  baseURL: "http://34.131.30.62:5000/api",
   headers: {
     "Content-Type": "application/json",
   },
