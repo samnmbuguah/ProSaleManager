cdc86652e02d2083f34dade045c3b08a921d8511
Samuel Mbugua Nyambura <samnmbuguah@gmail.com>
2025-05-29 12:54:55 +0300

    :bug: Fix: Add checkSession dependency to useEffect in ExpensesPage

--- client/src/pages/ExpensesPage.tsx
+++ client/src/pages/ExpensesPage.tsx
@@ -24,7 +24,7 @@ export default function ExpensesPage() {
     if (!isAuthenticated && !authLoading) {
       checkSession();
     }
-  }, [isAuthenticated, authLoading]);
+  }, [isAuthenticated, authLoading, checkSession]);
 
   useEffect(() => {
     if (!authLoading && !isAuthenticated) {
